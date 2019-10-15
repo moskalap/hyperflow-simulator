ad81cf15e744f0757ffe298b170e5342e7f0eae2
Przemysław Moskała <przemyslaw.moskala@synerise.com>
2019-10-15 18:51:47 +0200

    Add executor service with datacenter init

--- src/main/scala/pl/edu/agh/hyperflow/simulator/config/Config.scala
+++ src/main/scala/pl/edu/agh/hyperflow/simulator/config/Config.scala
@@ -1,9 +1,16 @@
 package pl.edu.agh.hyperflow.simulator.config
 
+import java.util
+
 import com.typesafe.config.ConfigFactory
+import org.cloudbus.cloudsim.Storage
 import org.cloudbus.cloudsim.container.containerProvisioners.{ContainerBwProvisionerSimple, ContainerPe, ContainerRamProvisionerSimple, CotainerPeProvisionerSimple}
 import org.cloudbus.cloudsim.container.containerVmProvisioners.{ContainerVmBwProvisioner, ContainerVmBwProvisionerSimple, ContainerVmPe, ContainerVmPeProvisioner, ContainerVmPeProvisionerSimple, ContainerVmRamProvisionerSimple}
+import org.cloudbus.cloudsim.container.hostSelectionPolicies.HostSelectionPolicyFirstFit
+import org.cloudbus.cloudsim.container.resourceAllocatorMigrationEnabled.PowerContainerVmAllocationPolicyMigrationAbstractHostSelection
+import org.cloudbus.cloudsim.container.resourceAllocators.{ContainerVmAllocationPolicy, PowerContainerAllocationPolicySimple}
 import org.cloudbus.cloudsim.container.schedulers.{ContainerCloudletScheduler, ContainerCloudletSchedulerDynamicWorkload, ContainerScheduler, ContainerSchedulerTimeSharedOverSubscription, ContainerVmSchedulerTimeSharedOverSubscription}
+import org.cloudbus.cloudsim.container.vmSelectionPolicies.{PowerContainerVmSelectionPolicy, PowerContainerVmSelectionPolicyMaximumUsage}
 import org.cloudbus.cloudsim.power.models.PowerModelSpecPowerHpProLiantMl110G4Xeon3040
 
 import collection.mutable._
@@ -24,6 +31,32 @@ object Config {
   }
 
   object Cloud {
+    object Policies {
+      val container = new PowerContainerAllocationPolicySimple
+      val vm: PowerContainerVmSelectionPolicy = new PowerContainerVmSelectionPolicyMaximumUsage
+      val host = new HostSelectionPolicyFirstFit
+      val overUtilizationThreshold = 0.8
+      val underUtilizationThreshold = 0.7
+    }
+
+    object Datacenter {
+      val containerStartupDelay: Double = 0.1
+      val vmStartupDelay: Double = 0.1
+      val logAddres: String = "./exp"
+      var experimentName: String = "ContainerCloudSimExample_1"
+      var schedulingInterval: Double = 10
+      val storageList: util.List[Storage] = new util.ArrayList[Storage]()
+      val name: String = "datacenter"
+      val costPerBw: Double = 0
+      val costPerStorage: Double = 0.001
+      val costPerMem: Double = 0.05
+      val costPerSec: Double = 3.0
+      val timezone: Double = 10.0d
+      val vmm: String = "Xem"
+      val os: String = "Linux"
+      val architecture: String = "x86"
+    }
+
 
     object Host {
       val ram = new ContainerVmRamProvisionerSimple(1000)
@@ -47,17 +80,15 @@ object Config {
       val powerModel = new PowerModelSpecPowerHpProLiantMl110G4Xeon3040()
     }
     object Container {
-
-      val pes: Int = 100
-
+      val pes: Int = 10
       val schedulingInterval: Double = 10
       val vmm: String = "xen"
-      val size: Long = 100000
-      val bw: Long = 100000
-      val ram: Int = 1000
-      val mips: Double = 2000.0
-      val ramProvisioner = new ContainerRamProvisionerSimple(1000)
-      val bandWidthProvisioner = new ContainerBwProvisionerSimple(100000)
+      val size: Long = 100
+      val bw: Long = 1000
+      val ram: Int = 100
+      val mips: Double = 200.0
+      val ramProvisioner = new ContainerRamProvisionerSimple(100)
+      val bandWidthProvisioner = new ContainerBwProvisionerSimple(1000)
       val peList = scala.collection.JavaConverters.seqAsJavaList((1 to 3) map (i => new ContainerPe(i, new CotainerPeProvisionerSimple(2000.0))))
       val scheduler: ContainerCloudletScheduler = new ContainerCloudletSchedulerDynamicWorkload(this.mips, this.pes)
       val powerModel = new PowerModelSpecPowerHpProLiantMl110G4Xeon3040()
--- src/main/scala/pl/edu/agh/hyperflow/simulator/services/ExecutorService.scala
+++ src/main/scala/pl/edu/agh/hyperflow/simulator/services/ExecutorService.scala
@@ -1,54 +1,89 @@
 package pl.edu.agh.hyperflow.simulator.services
 
+import java.util
 import java.util.Calendar
 
 import pl.edu.agh.hyperflow.simulator.config.Config.Cloud
 import akka.event.LoggingAdapter
-import ch.qos.logback.classic.Logger
-import org.cloudbus.cloudsim.{Cloudlet, DatacenterBroker}
-import org.cloudbus.cloudsim.container.core.{Container, ContainerDatacenterCharacteristics, ContainerHost, PowerContainer, PowerContainerHostUtilizationHistory, PowerContainerVm}
+import org.cloudbus.cloudsim.{Cloudlet, DatacenterBroker, UtilizationModelFull, UtilizationModelNull}
+import org.cloudbus.cloudsim.container.core.{Container, ContainerCloudlet, ContainerDatacenterBroker, ContainerDatacenterCharacteristics, ContainerHost, PowerContainer, PowerContainerDatacenterCM, PowerContainerHostUtilizationHistory, PowerContainerVm}
+import org.cloudbus.cloudsim.container.resourceAllocatorMigrationEnabled.PowerContainerVmAllocationPolicyMigrationAbstractHostSelection
 import org.cloudbus.cloudsim.container.utils.IDs
 import org.cloudbus.cloudsim.core.CloudSim
 import pl.edu.agh.hyperflow.simulator.hyperflow.Executable.Executable
 
 
-
 class ExecutorService(implicit logger: LoggingAdapter) {
+
   def addExecutable(ex: Executable) = {
     logger.info(s"add executable ${ex.toString}")
+
+    val newCloudlet = new ContainerCloudlet(ex.procId, Math.random().toInt, 0, 0, 0, utlizationNull, utlizationNull, utlizationNull, false)
+    val cloudlets = new util.ArrayList[ContainerCloudlet]()
+    broker.submitCloudletList(cloudlets)
   }
 
 
   logger.info("Initializing executor service")
   CloudSim.init(1, Calendar.getInstance(), true)
-  val broker = new DatacenterBroker("broker")
-  val hosts = this.createHosts(5)
-  val vms = this.createVms(10)
-  val containers = this.createContainers(20)
-
-//  def createDatacenter() = {
-//    val datacenterCharacteristics = new ContainerDatacenterCharacteristics(
-//      Cloud.Datacenter.architecture,
-//      Cloud.Datacenter.os,
-//      Cloud.Datacenter.vmm,
-//      this.hosts,
-//      Cloud.Datacenter.timezone,
-//      Cloud.Datacenter.costPerSec,
-//      Cloud.Datacenter.costPerMem,
-//      Cloud.Datacenter.costPerStorage,
-//      Cloud.Datacenter.costPerBw
-//    )
-//  }
 
 
-//  val datacenter = createDatacenter()
+  val broker = new ContainerDatacenterBroker("broker", Cloud.Policies.overUtilizationThreshold)
+  val hosts = this.createHosts(1)
+  val vms = this.createVms(1)
+  val containers = this.createContainers(1)
+  val datacenter = createDatacenter()
+
+
+  val utlizationNull = new UtilizationModelFull
+  val initCloudlet = new ContainerCloudlet(IDs.pollId(classOf[ContainerCloudlet]), 1000000000, 100, 100, 10, utlizationNull, utlizationNull, utlizationNull, true)
+  val initCloudlet1 = new ContainerCloudlet(IDs.pollId(classOf[ContainerCloudlet]), 1000000000, 100, 100, 10, utlizationNull, utlizationNull, utlizationNull, true)
+  val cloudlets = new util.ArrayList[ContainerCloudlet]()
+  cloudlets.add(initCloudlet)
+  cloudlets.add(initCloudlet1)
+  broker.submitCloudletList(cloudlets)
+  broker.submitContainerList(containers)
+  broker.submitVmList(vms)
+
+
+  CloudSim.startSimulation()
+
+  def createDatacenter() = {
+    val datacenterCharacteristics = new ContainerDatacenterCharacteristics(
+      Cloud.Datacenter.architecture,
+      Cloud.Datacenter.os,
+      Cloud.Datacenter.vmm,
+      this.hosts,
+      Cloud.Datacenter.timezone,
+      Cloud.Datacenter.costPerSec,
+      Cloud.Datacenter.costPerMem,
+      Cloud.Datacenter.costPerStorage,
+      Cloud.Datacenter.costPerBw
+    )
+
+    new PowerContainerDatacenterCM(
+      Cloud.Datacenter.name,
+      datacenterCharacteristics,
+      new PowerContainerVmAllocationPolicyMigrationAbstractHostSelection(hosts, Cloud.Policies.vm, Cloud.Policies.host, Cloud.Policies.overUtilizationThreshold, Cloud.Policies.underUtilizationThreshold),
+      Cloud.Policies.container,
+      Cloud.Datacenter.storageList,
+      Cloud.Datacenter.schedulingInterval,
+      Cloud.Datacenter.experimentName,
+      Cloud.Datacenter.logAddres,
+      Cloud.Datacenter.vmStartupDelay,
+      Cloud.Datacenter.containerStartupDelay
+    )
+  }
+
+
+  //  val datacenter = createDatacenter()
 
   def createHosts(numberOfHost: Int) = {
-    (1 to numberOfHost) map (i => new PowerContainerHostUtilizationHistory(i, Cloud.Host.ram, Cloud.Host.bandWidth, 1000000, Cloud.Host.peList, Cloud.Host.scheduler, Cloud.Host.powerModel ))
+    scala.collection.JavaConverters.seqAsJavaList((1 to numberOfHost) map (i => new PowerContainerHostUtilizationHistory(i, Cloud.Host.ram, Cloud.Host.bandWidth, 1000000, Cloud.Host.peList, Cloud.Host.scheduler, Cloud.Host.powerModel)))
   }
 
   def createVms(numberOfVms: Int) = {
-    (1 to numberOfVms) map (i => new PowerContainerVm(
+    scala.collection.JavaConverters.seqAsJavaList((1 to numberOfVms) map (i => new PowerContainerVm(
       i,
       this.broker.getId,
       Cloud.VM.mips,
@@ -60,11 +95,11 @@ class ExecutorService(implicit logger: LoggingAdapter) {
       Cloud.VM.ramProvisioner,
       Cloud.VM.bandWidthProvisioner,
       Cloud.VM.peList,
-      Cloud.VM.schedulingInterval))
+      Cloud.VM.schedulingInterval)))
   }
 
-  def createContainers(numberOfContainers: Int): Unit = {
-    (1 to numberOfContainers) map ( i => new PowerContainer(
+  def createContainers(numberOfContainers: Int) = {
+    scala.collection.JavaConverters.seqAsJavaList((1 to numberOfContainers) map (i => new PowerContainer(
       IDs.pollId(classOf[Container]),
       this.broker.getId,
       Cloud.Container.mips,
@@ -75,16 +110,7 @@ class ExecutorService(implicit logger: LoggingAdapter) {
       Cloud.Container.vmm,
       Cloud.Container.scheduler,
       Cloud.Container.schedulingInterval,
-      
-    ))
+
+    )))
   }
-
-  def createCloudLet(): Unit = {
-
-  }
-
-
-
-
-
 }
